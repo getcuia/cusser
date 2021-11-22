f9625e3e4f6c569a705beeb623f2713323f74780
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-22 17:39:30 -0300

    Add the basic wrapper

--- src/cusser/__init__.py
+++ src/cusser/__init__.py
@@ -1,1 +1,19 @@
+"""A curses wrapper that understands ANSI escape code sequences."""
+
+
+from __future__ import annotations
+
+import curses
+from dataclasses import dataclass
+
 __version__ = "0.1.0"
+
+
+__all__ = ["Cusser"]
+
+
+@dataclass
+class Cusser:
+    """A curses wrapper that understands ANSI escape code sequences."""
+
+    window: curses._CursesWindow
--- tests/test_cusser.py
+++ tests/test_cusser.py
@@ -1,5 +1,22 @@
-from cusser import __version__
+"""General tests."""
+
+import curses
+
+from cusser import Cusser, __version__
 
 
 def test_version():
+    """Ensure the version is correct."""
     assert __version__ == "0.1.0"
+
+
+def test_cusser():
+    """Ensure the wrapper works."""
+
+    def _app(stdscr):
+        text = "\033[1;5;33mHello \033[2mWorld\033[0m!"
+        stdscr.addstr(text)
+        stdscr.refresh()
+        stdscr.getch()
+
+    _app(curses.initscr())
