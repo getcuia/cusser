4e1c7ff7371f1d59aec032169cdb39a57edc40dc
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-22 18:41:21 -0300

    Do a more envolved testing with attributes

--- src/cusser/__main__.py
+++ src/cusser/__main__.py
@@ -2,16 +2,36 @@
 
 
 import curses
+from functools import reduce
+from typing import Callable, Text
 
+from stransi import Ansi
 
 from . import Cusser
 
-
 if __name__ == "__main__":
+
+    def _tag(start: Text, end: Text = "\033[0m") -> Callable[[Text], Ansi]:
+        def decorator(text: Text) -> Ansi:
+            return Ansi(f"{start}{text}{end}")
+
+        return decorator
+
+    normal = _tag("\033[0m", "")
+    bold = _tag("\033[1m", "\033[22m")
+    dim = _tag("\033[2m", "\033[22m")
+    italic = _tag("\033[3m", "\033[23m")
+    underline = _tag("\033[4m", "\033[24m")
+    blink = _tag("\033[5m", "\033[25m")
+    reverse = _tag("\033[7m", "\033[27m")
+    hidden = _tag("\033[8m", "\033[28m")
+
+    ALL_STYLES = (normal, bold, dim, italic, underline, blink, reverse, hidden)
 
     def _app(stdscr):
         stdscr = Cusser(stdscr)
-        text = "\033[1;5;33mHello \033[2mWorld\033[0m!"
+        message = "The quick brown fox jumps over the lazy dog"
+        text = reduce(lambda acc, style: acc + style(message) + "\n", ALL_STYLES, "")
         stdscr.addstr(text)
         stdscr.refresh()
         stdscr.getch()
--- tests/test_cusser.py
+++ tests/test_cusser.py
@@ -1,6 +1,10 @@
 """General tests."""
 
 import curses
+from functools import reduce
+from typing import Callable, Text
+
+from stransi import Ansi
 
 from cusser import Cusser, __version__
 
@@ -13,9 +17,27 @@ def test_version():
 def test_cusser():
     """Ensure the wrapper works."""
 
+    def _tag(start: Text, end: Text = "\033[0m") -> Callable[[Text], Ansi]:
+        def decorator(text: Text) -> Ansi:
+            return Ansi(f"{start}{text}{end}")
+
+        return decorator
+
+    normal = _tag("\033[0m", "")
+    bold = _tag("\033[1m", "\033[22m")
+    dim = _tag("\033[2m", "\033[22m")
+    italic = _tag("\033[3m", "\033[23m")
+    underline = _tag("\033[4m", "\033[24m")
+    blink = _tag("\033[5m", "\033[25m")
+    reverse = _tag("\033[7m", "\033[27m")
+    hidden = _tag("\033[8m", "\033[28m")
+
+    ALL_STYLES = (normal, bold, dim, italic, underline, blink, reverse, hidden)
+
     def _app(stdscr):
         stdscr = Cusser(stdscr)
-        text = "\033[1;5;33mHello \033[2mWorld\033[0m!"
+        message = "The quick brown fox jumps over the lazy dog"
+        text = reduce(lambda acc, style: acc + style(message) + "\n", ALL_STYLES, "")
         stdscr.addstr(text)
         stdscr.refresh()
         stdscr.getch()
