e6391366ce1cb73cbec81c73797b3efe303b46bc
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-29 16:47:52 -0300

    Simplify color manager tests

--- tests/test_color_manager.py
+++ tests/test_color_manager.py
@@ -1,23 +1,13 @@
-"""Tests for the color bag class."""
+"""Tests for the color manager class."""
 
-import curses
 
 import ochre
-import pytest
 
 from cusser.color_manager import ColorManager
 
 
-@pytest.fixture
-def stdscr():
-    """Return a curses screen."""
-    stdscr = curses.initscr()
-    curses.start_color()
-    yield stdscr
-
-
-def test_color_manager_add_color(stdscr):
-    """Test adding a color to the color bag."""
+def test_color_manager_add_color():
+    """Test adding a color to the color manager."""
     color_manager = ColorManager()
     color_manager.add(ochre.Hex("#ff0000"))
     assert list(color_manager.colors) == [ochre.RGB(1, 0, 0)]
@@ -25,8 +15,8 @@ def test_color_manager_add_color(stdscr):
     assert ochre.RGB(1, 0, 0) in color_manager
 
 
-def test_color_manager_discard_color(stdscr):
-    """Test discarding a color from the color bag."""
+def test_color_manager_discard_color():
+    """Test discarding a color from the color manager."""
     color_manager = ColorManager()
     color_manager.add(ochre.Hex("#ff0000"))
     color_manager.discard(ochre.Hex("#ff0000"))
