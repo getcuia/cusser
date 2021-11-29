bbaeb1cd285484c403faaa3b87d4f13e7fc35b61
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-29 16:48:16 -0300

    Minimal implementation to make tests work

--- src/cusser/color_manager.py
+++ src/cusser/color_manager.py
@@ -1,8 +1,8 @@
 """Utilities for managing colors and color pairs in curses."""
 
 
-from dataclasses import dataclass
-from typing import Optional
+from dataclasses import dataclass, field
+from typing import Iterable, Optional
 
 import ochre
 
@@ -19,3 +19,30 @@ class ColorPair:
 @dataclass
 class ColorManager:
     """A class for managing curses colors and color pairs."""
+
+    color_indices: dict[ochre.Color, int] = field(default_factory=dict)
+
+    def add(self, color: ochre.Color) -> None:
+        """Register a color with the color manager."""
+        h = hex(color)
+        if h not in self.color_indices:
+            self.color_indices[h] = len(self.color_indices)
+
+    def discard(self, color: ochre.Color) -> None:
+        """Unregister a color from the color manager."""
+        h = hex(color)
+        if h in self.color_indices:
+            del self.color_indices[h]
+
+    @property
+    def colors(self) -> Iterable[ochre.Color]:
+        """Return all colors currently registered."""
+        return map(ochre.Hex, self.color_indices.keys())
+
+    def __len__(self) -> int:
+        """Return the number of colors currently registered."""
+        return len(self.color_indices)
+
+    def __iter__(self) -> Iterable[ochre.Color]:
+        """Return an iterator over all colors currently registered."""
+        return self.colors
