7944a371344d62b6de882af5a02724ba4095cc16
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-12-01 14:33:18 -0300

    Don't assume we always have colors

--- src/cusser/__init__.py
+++ src/cusser/__init__.py
@@ -24,14 +24,16 @@ __all__ = ["Cusser"]
 
 def on_add_color(color: ochre.Color, manager: ColorManager) -> None:
     """Initialize a color when it is added to the color manager."""
-    if curses.can_change_color():
+    if curses.has_colors() and curses.can_change_color():
         color = color.rgb
-        curses.init_color(
+        return curses.init_color(
             manager[color],
             int(1000 * color.red),
             int(1000 * color.green),
             int(1000 * color.blue),
         )
+
+    raise RuntimeError("Cannot initialize color")
 
 
 def on_add_pair(pair: ColorPair, manager: ColorManager) -> None:
